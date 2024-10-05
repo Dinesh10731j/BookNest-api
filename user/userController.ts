2e25d880b1b5a4b85e6e75482228eacf32510859
Dinesh10731j <dinesh1122@gmail.com>
2024-10-05 12:35:33 +0545

    usersignup endpoint  completed

--- user/userController.ts
+++ user/userController.ts
@@ -1,6 +1,7 @@
 
 import express from "express"
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 const createUser = async (req:express.Request,res:express.Response,next:express.NextFunction)=>{
 const {name,email,password} = req.body;
 if(!name || !email || !password){
@@ -8,6 +9,20 @@ if(!name || !email || !password){
 
     return next(error);
 }
+
+//checking if userAlready exits or not
+
+
+const alreadyExists = await userModel.findOne({email});
+
+if(alreadyExists){
+    const error = createHttpError(400,'User Already exists');
+
+
+    next(error);
+}
+
+
 
 }
 
